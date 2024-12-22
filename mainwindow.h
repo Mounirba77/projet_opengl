607302b8779547452b163d17cbdefc1a73f2b4c6
Asma Jadlaoui <79285139+jadasma@users.noreply.github.com>
2024-12-22 16:10:29 +0100

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QString>  // Inclure QString pour gérer les chaînes de caractères
+#include <QLabel>   // Inclure QLabel pour afficher les coordonnées
 
 namespace Ui {
 class MainWindow;
@@ -10,22 +11,29 @@ class MainWindow;
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+    Q_OBJECT // Ajout de cette macro pour les signaux/slots
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
-
 public slots:
+    void updateCoordinates(double x, double y, double z); // Slot pour mettre à jour les coordonnées
 
-
-private slots:
+    // Déclaration des autres slots pour les sliders
     void on_verticalSlider_actionTriggered(int action);
     void on_verticalSlider_2_actionTriggered(int action);
     void on_verticalSlider_3_actionTriggered(int action);
+
+private:
+    Ui::MainWindow *ui;
+    QLabel *coordinatesLabel; // Un QLabel pour afficher les coordonnées
+
+    // Variables pour stocker les coordonnées des sliders
+    double xCoordinate, yCoordinate, zCoordinate;
+
+    // Initial positions for sliders (assuming initial value is 0)
+    int initialSliderValue;
 };
 
 #endif // MAINWINDOW_H
